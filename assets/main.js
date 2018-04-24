36fe6a438f09be32b7edf324db06afbcbd776e1f
Daniel Ortiz <d.ortiz@mozio.com>
2018-04-24 16:13:58 -0500

    add initial sidebar implementation

---
+++ assets/main.js
@@ -0,0 +1,44 @@
+$(function() {
+  const DESKTOP_SIDEBAR = 'desktop';
+  const MOBILE_SIDEBAR = 'mobile';
+
+  const isMobile = () => $(window).width() <= 768;
+
+  let activeSidebar;
+
+  const showDesktopSidebar = () => {
+    $('#mobile-sidebar').hide();
+    $('#desktop-sidebar').show();
+    activeSidebar = DESKTOP_SIDEBAR;
+  }
+
+  const showMobileSidebar = () => {
+    $('#desktop-sidebar').hide();
+    $('#mobile-sidebar').show();
+    activeSidebar = MOBILE_SIDEBAR;
+  }
+
+  const animations = {
+    [DESKTOP_SIDEBAR]: () => {
+      showMobileSidebar();
+    },
+    [MOBILE_SIDEBAR]: () => {
+      showDesktopSidebar();
+    }
+  };
+
+  const initSidebar = () => {
+    return isMobile()
+      ? showMobileSidebar()
+      : showDesktopSidebar()
+  }
+
+  $('.sidebar-toggler').on('click', () => animations[activeSidebar]());
+
+  initSidebar();
+  $('.dropdown').dropdown();
+
+  $(window).resize(() => {
+    initSidebar();
+  });
+});
